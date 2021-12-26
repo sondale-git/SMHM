d2f1e2ab277c5ac0edc0f41003bf9e98a4e11b2b
sondale-git <61547150+sondale-git@users.noreply.github.com>
2021-12-26 20:38:23 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='SMHM',
+    version='0.1.0',
+    description='Swiss Municipalities Historical Mapper',
+    author='sondale-git',
+    long_description=long_description
+    long_description_content_type="text/markdown"
+    license="MIT",
+    url='https://github.com/sondale-git/SMHM'
+    packages=['SMHM'],  #same as name
+    # install_requires = ["pandas"], #external packages as dependencies
+)
